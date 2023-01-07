da0b765d5955259b061710176ac3dc13b5b54662
Marcos <marcoscanette1@gmail.com>
2023-01-06 22:57:35 -0300

    asd

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 const { Client, LocalAuth} = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 
-const client = new Client({
-    authStrategy: new LocalAuth(),
-	    puppeteer: {
-		args: ['--no-sandbox']
-	}
-});
+const client = new Client();
 
 client.on('qr', (qr) => {
     qrcode.generate(qr, {small: true});
